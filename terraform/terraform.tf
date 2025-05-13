3131efba178c717786bb4c79d5bd95aca7db7312
thinh tu <khangthinhproc@gmail.com>
2025-05-13 13:13:18 +0700

    update bucket name and cluster name

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "rg3-gitops"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-southeast-7"
   }
 
   required_version = "~> 1.6.3"
